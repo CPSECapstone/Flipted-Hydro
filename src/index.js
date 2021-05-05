14838a7dcf782faf479ce9e2bfaced56d99575e7
samiur98 <sam@Shahs-MacBook-Pro.local>
2021-05-04 18:49:13 -0700

    Changing Nav Links

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename='/cpsecapstone.github.io/Flipted-Hydro'>
+  <BrowserRouter basename='/Flipted-Hydro'>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
